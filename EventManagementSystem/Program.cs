eb2c540e69cc0415457d0ccc494bcfe63342f3e7
Saloni260712 <patelsaloni.0711@gmail.com>
2024-02-28 19:03:31 -0800

    program file changes

--- EventManagementSystem/Program.cs
+++ EventManagementSystem/Program.cs
@@ -11,8 +11,8 @@ namespace EventManagementSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new FormEventManipulation());
-            Application.Run(new FormLogIn());
+            Application.Run(new FormEventManipulation());
+            //Application.Run(new FormLogIn());
 
         }
     }
