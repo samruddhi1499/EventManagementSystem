886dd34c6b7b8ea264dacd6111f5ea874dd6fd6b
samruddhi1499 <samchavanxiv9@gmail.com>
2024-03-05 14:44:21 -0500

    Added view

---
+++ EventManagementSystem/FormAdminViewEvent.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace EventManagementSystem
+{
+    partial class FormAdminViewEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            eventViewList = new ListBox();
+            btnCloseView = new Button();
+            SuspendLayout();
+            // 
+            // eventViewList
+            // 
+            eventViewList.FormattingEnabled = true;
+            eventViewList.Location = new Point(12, 18);
+            eventViewList.Name = "eventViewList";
+            eventViewList.Size = new Size(340, 324);
+            eventViewList.TabIndex = 0;
+            // 
+            // btnCloseView
+            // 
+            btnCloseView.Location = new Point(94, 360);
+            btnCloseView.Name = "btnCloseView";
+            btnCloseView.Size = new Size(155, 29);
+            btnCloseView.TabIndex = 1;
+            btnCloseView.Text = "Close";
+            btnCloseView.UseVisualStyleBackColor = true;
+            btnCloseView.Click += btnCloseView_Click;
+            // 
+            // FormAdminViewEvent
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(364, 401);
+            Controls.Add(btnCloseView);
+            Controls.Add(eventViewList);
+            Name = "FormAdminViewEvent";
+            Text = "FormAdminViewEvent";
+            Load += FormAdminViewEvent_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox eventViewList;
+        private Button btnCloseView;
+    }
+}
---
+++ EventManagementSystem/FormAdminViewEvent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventManagementSystem
+{
+    public partial class FormAdminViewEvent : Form
+    {
+        public static string selectedEvent { get; set; }
+        public FormAdminViewEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAdminViewEvent_Load(object sender, EventArgs e)
+        {
+            ArrayList arrayList = FormEventManipulation.eventObjectList;
+
+            string[] arr = selectedEvent.Split('-');
+
+            string a = arr[0].Trim();
+
+            foreach (EventsClass array in arrayList)
+            {
+
+                EventsClass eventClass = (EventsClass)array;
+                string b = eventClass.EventName.ToString();
+                if (a.Equals(b))
+                {
+                    eventViewList.Items.Add("Event Name:  " + array.EventName.ToString());
+                    eventViewList.Items.Add("Event Date:  " + array.EventDate.ToString());
+                    eventViewList.Items.Add("Event Time:  " + array.EventTime.ToString());
+                    eventViewList.Items.Add("Event Location:  " + array.EventLocation.ToString());
+                    eventViewList.Items.Add("Event Description:  " + array.EventDescription.ToString());
+                    eventViewList.Items.Add("Event Capacity:  " + array.EventCapacity.ToString());
+                    eventViewList.Items.Add("Event Organizer:  " + array.EventEM.ToString());
+
+                    break;
+
+                }
+            }
+
+        }
+
+        public void getVal(string val)
+        {
+            selectedEvent = val;
+        }
+
+        private void btnCloseView_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
